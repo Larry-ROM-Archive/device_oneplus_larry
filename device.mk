5bac298825666482dae134ec1a09dcea1ac3a80a
amit-0i <amit0i+github@proton.me>
2026-05-30 19:13:16 +0530

    larry: build GameBar
    Signed-off-by: viaan-thakur <viaan4542@gmail.com>

--- device.mk
+++ device.mk
@@ -35,6 +35,9 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
+# GameBar
+$(call inherit-product-if-exists, packages/apps/GameBar/gamebar.mk)
+
 # NFC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_14/android.hardware.nfc.hce.xml \
