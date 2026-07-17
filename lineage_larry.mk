2443a63f97531b86ec9f8adf8ed894defc9bcc5c
Viaan Thakur <viaan4542@gmail.com>
2026-07-17 17:39:27 +0530

    larry: Adapt to MistOS

--- lineage_larry.mk
+++ lineage_larry.mk
@@ -15,12 +15,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from larry device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# LunarisAOSP Flags
-LUNARIS_BUILD_TYPE := OFFICIAL
+# MistOS Flags
+MISTOS_MAINTAINER := "NoEscape.exe"
 WITH_GMS := true
-TARGET_SUPPORTS_GOOGLE_TELEPHONY := true
-SURFACE_FLINGER_BOOST := true
-WITH_BCR := true
 TARGET_ENABLE_BLUR := true
 
 # Device identifier. This must come after all inclusions.
